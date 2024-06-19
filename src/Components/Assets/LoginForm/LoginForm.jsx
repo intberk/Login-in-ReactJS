9b637cf0a36dd13fd5be33f852b440810961e566
Intberk <intberkmen@gmail.com>
2024-06-19 23:59:33 +0300

    Over

--- src/Components/Assets/LoginForm/LoginForm.jsx
+++ src/Components/Assets/LoginForm/LoginForm.jsx
@@ -24,6 +24,7 @@ const LoginForm = () => {
                 <div className='register-link'>
                     <p>Don't have an account? <a href="#">Register</a></p>
                 </div>
+                <div className="not-equal-lifes"></div>
             </form>
         </div>
     );
